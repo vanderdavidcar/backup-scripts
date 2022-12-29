be1f7f4b87736c0bfbb498d5da0d6528e297cee4
vanderson <vander_davidcar@yahoo.com.br>
2022-12-29 16:04:27 -0300

    update net_conn.py

---
+++ net_conn.py
@@ -0,0 +1,40 @@
+"""
+Function Netmiko Connection
+"""
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+username = os.getenv("USERNAME")
+passwd = os.getenv("PASSWD")
+secret = os.getenv("SECRET")
+
+user_lab = os.getenv("USER_LAB")
+pass_lab = os.getenv("PASS_LAB")
+
+def netmiko_lab(ip):
+    return {
+            'device_type': 'cisco_ios',
+            'ip': ip,
+            'username': user_lab,
+            'password': pass_lab,
+             }
+
+def netmiko_a10(ip):
+    return {
+            'device_type': 'a10',
+            'ip': ip,
+            'username': username,
+            'password': passwd,
+            'secret': secret
+             }
+
+# IOS-XR module works from many softwares as IOS, IOS-XR, NXOS, DellOS10
+def netmiko_iosxr(ip):
+    return {
+            'device_type': 'cisco_xr',
+            'ip': ip,
+            'username': username,
+            'password': passwd,
+             }
